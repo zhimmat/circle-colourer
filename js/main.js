aa545ea989a1ae98552b6ce93686e8c8a3a2498a
zhimmat <zeyad.himmat@live.com>
2016-03-23 12:42:10 -0400

    last code in js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ var $html = $('html');
 var $ball = $('div');
 var $picker = $('form');
 var $input = $('#pick-color');
+
+$picker.on('change', function() {
+  console.log('works');
+  $ball.css('background-color', $input.val());
+});
